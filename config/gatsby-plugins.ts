13b3480b06a85cbe96e6cce19f28a25750327f84
ktnkk <ktnkk@pm.me>
2022-01-10 22:06:24 +0900

    feat(types): introduce gatsby-plugin-typegen

--- config/gatsby-plugins.ts
+++ config/gatsby-plugins.ts
@@ -1,5 +1,12 @@
 import { GatsbyConfig } from "gatsby";
 
-const plugins: GatsbyConfig["plugins"] = [];
+const plugins: GatsbyConfig["plugins"] = [
+  {
+    resolve: "gatsby-plugin-typegen",
+    options: {
+      outputPath: "types/gatsby-types.d.ts",
+    },
+  },
+];
 
 export default plugins;
